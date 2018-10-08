69a3ffd2702c9786a32a2e823d3094d26c05f26e
Will Boyce <me@willboyce.com>
2018-10-08 15:08:02 +0100

    middleware-handler: Use custom, simpler, middleware handler
    Change-type: major
    Signed-off-by: Will Boyce <will@resin.io>

--- src/index.ts
+++ src/index.ts
@@ -17,14 +17,10 @@ import basicAuthParser = require('basic-auth-parser');
 import * as Promise from 'bluebird';
 import { EventEmitter } from 'events';
 import * as http from 'http';
-import MiddlewareHandler = require('middleware-handler');
 import * as net from 'net';
 import * as url from 'url';
 
-interface MiddlewareHandlerAsync extends MiddlewareHandler {
-	handleAsync(args?: any[]): Promise<void>;
-}
-MiddlewareHandler.prototype = Promise.promisifyAll(MiddlewareHandler.prototype) as MiddlewareHandlerAsync;
+export type Middleware = (req: Request, cltSocket: net.Socket, head: Buffer, next: () => void) => void;
 
 export type NetConnectPromise = (port: number, hostname: string, cltSocket: net.Socket, req: Request) => Promise<net.Socket>;
 
@@ -77,18 +73,18 @@ export class Request extends http.IncomingMessage {
 }
 
 export class Tunnel extends EventEmitter {
-	private server = http.createServer((_req, res) => {
+	private readonly stack: Array<Middleware> = [];
+	private readonly server = http.createServer((_req, res) => {
 		res.writeHead(405, {'Content-Type': 'text/plain'});
 		res.end('Method not allowed');
 	});
-	private middleware = new MiddlewareHandler() as MiddlewareHandlerAsync;
 
 	constructor() {
 		super();
 
 		this.use(basicAuth);
 		this.server.on('connect', (req: Request, cltSocket: net.Socket, head: Buffer) =>
-			this.middleware.handleAsync([ req, cltSocket, head ])
+			this.handleMiddleware(req, cltSocket, head)
 			.then(() => {
 				const { hostname, port } = url.parse(`http://${req.url}`);
 				if (hostname == null || port == null) {
@@ -105,11 +101,38 @@ export class Tunnel extends EventEmitter {
 				.then(() =>
 					this.emit('connect', hostname, port, head)
 				);
-			}).catch((err) => {
+			}).catch((err: Error) => {
 				this.emit('error', err);
 				cltSocket.destroy();
 			})
 		);
+	}
+
+	use(middleware: Middleware) {
+		this.stack.push(middleware);
+	}
+
+	private handleMiddleware(req: Request, cltSocket: net.Socket, head: Buffer): Promise<void> {
+		return new Promise((resolve, reject) => {
+			let index = 0;
+
+			const next = (err?: Error) => {
+				const middleware = this.stack[ index++ ];
+				if (err != null) {
+					reject(err);
+				} else if (middleware != null) {
+					try {
+						middleware(req, cltSocket, head, next);
+					} catch (err) {
+						reject(err);
+					}
+				} else {
+					resolve()
+				}
+			};
+
+			next();
+		});
 	}
 
 	connect(port: number, host: string, _cltSocket: net.Socket, _req: Request): Promise<net.Socket> {
@@ -119,16 +142,12 @@ export class Tunnel extends EventEmitter {
 			socket.on('error', reject);
 		});
 	}
-	use: (middleware: Middleware) => void = this.middleware.use.bind(this.middleware);
+
 	listen: (port: number | string, callback?: (err: any, result?: any) => void) => this = this.server.listen.bind(this.server);
 	close: (callback?: (error?: Error) => void) => this = this.server.close.bind(this.server);
 }
 
-// for backwards compatibility
-export const createTunnel = (): Tunnel => new Tunnel();
-
 // Proxy authorization middleware for http tunnel.
-export type Middleware = (req: Request, cltSocket: net.Socket, head: Buffer, next: () => void) => void;
 export const basicAuth: Middleware = (req, _cltSocket, _head, next) => {
 	const proxyAuth = req.headers['proxy-authorization'];
 	if (proxyAuth != null) {
